1227dc2e12388b22ba3744fa5b355b5af65b69a2
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-30 14:18:23 +0700

    feat(JS): variables declaration for the html's elements and data lists

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// list of jurusan and matakuliah
+let jurusan = []
+let matakuliah = []
+
+// getting each element from html
+const matkul_list = document.getElementById('list-body')
+const upload_button = document.getElementById('upload-button')
+const upload_type = document.getElementById('upload-type')
+const upload_file = document.getElementById('json-file')
+const reset_button = document.getElementById('reset-button')
+const constraint_jurusan = document.getElementById('jurusan')
+const constraint_sksmin = document.getElementById('sks-min')
+const constraint_sksmax = document.getElementById('sks-max')
+const constraint_semester = document.getElementById('semester')
+const search_button = document.getElementById('search')
